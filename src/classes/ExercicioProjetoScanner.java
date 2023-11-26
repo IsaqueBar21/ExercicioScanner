6a48cbff4a2cf8cd4c7b3b696df97b9aaff7cf9f
Isaque <isaquebarbosa@outlook.com>
2023-11-26 11:09:04 -0300

    primeiro commit

---
+++ src/classes/ExercicioProjetoScanner.java
@@ -0,0 +1,22 @@
+package classes;
+
+import java.util.Scanner;
+
+public class ExercicioProjetoScanner {
+
+	public static void main(String[] args) {
+		// entrada
+		int largura , comprimento, altura, volume;
+		
+		//criando um objeto da classe: Scanner
+		Scanner entrada = new Scanner(System.in);
+		
+		//entradas
+		System.out.println("Entre com o valor da largura: ");
+		largura = entrada.nextInt();
+		
+		
+
+	}
+
+}
